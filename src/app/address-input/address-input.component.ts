24eb31423a0805cb29047e47ef09741755377588
FabC77 <fabien.chambert@gmail.com>
2024-08-22 14:41:13 +0200

    wip geo

--- src/app/address-input/address-input.component.ts
+++ src/app/address-input/address-input.component.ts
@@ -1,26 +1,14 @@
 import { Component } from '@angular/core';
 import { environment } from '../../environment';
-import { GeoapifyGeocoderAutocompleteModule } from '@geoapify/angular-geocoder-autocomplete';
 
 @Component({
   selector: 'app-address-input',
   standalone: true,
-  imports: [GeoapifyGeocoderAutocompleteModule.withConfig('ff')],
+  imports: [],
   templateUrl: './address-input.component.html',
-  styleUrls: ['/node_modules/@geoapify/geocoder-autocomplete/styles/round-borders.css', './address-input.component.scss']
+  styleUrls: [ './address-input.component.scss']
 })
 export class AddressInputComponent {
-  onUserInput(input: string): void {
-    // Capture and react to user input
-    console.log('User Input:', input);
-  }
-  onSuggestionsChange(suggestions: GeoJSON.Feature[]): void {
-    // Handle the updated suggestions list
-    console.log('Suggestions:', suggestions);
-  }
-  placeSelected(place: any) {
-    console.log('Selected Place:', place);
-  }
 
 
 }
