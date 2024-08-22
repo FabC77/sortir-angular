778e3b9bb226ade46c60fa056892897c7f9dac26
FabC77 <fabien.chambert@gmail.com>
2024-08-22 15:07:17 +0200

    wip geo

--- src/app/address-input/address-input.component.ts
+++ src/app/address-input/address-input.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { environment } from '../../environment';
+import { GeocoderAutocomplete, GeocoderAutocompleteOptions } from '@geoapify/geocoder-autocomplete';
 
 @Component({
   selector: 'app-address-input',
   standalone: true,
   imports: [],
   templateUrl: './address-input.component.html',
-  styleUrls: [ './address-input.component.scss']
+  styleUrls: ["/node_modules/@geoapify/geocoder-autocomplete/styles/round-borders.css", './address-input.component.scss']
 })
-export class AddressInputComponent {
+export class AddressInputComponent implements AfterViewInit {
 
 
+  @ViewChild('autocompleteContainer', { static: true }) autocompleteContainer!: ElementRef;
+  private geocoderAutocomplete!: GeocoderAutocomplete;
+
+  ngAfterViewInit(): void {
+    const container = this.autocompleteContainer.nativeElement;
+
+    // Customize your Geocoder-Autocomplete options
+    const options: GeocoderAutocompleteOptions = {
+      // Add your options here
+      lang: 'fr',
+
+    };
+
+    // Create an instance of Geocoder-Autocomplete
+    this.geocoderAutocomplete = new GeocoderAutocomplete(container, options);
+  }
 }
